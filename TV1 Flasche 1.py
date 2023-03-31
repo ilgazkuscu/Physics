513639cef6a1b88ce8d84b8e43fd2ee7eae8e347
ilgazkuscu <40474649+ilgazkuscu@users.noreply.github.com>
2023-03-31 23:29:39 +0200

    Add files via upload
    +AKU Versuch Flasche 1 & 2 Plots

---
+++ TV1 Flasche 1.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import numpy as np
+
+#Fitting function
+def func(x, a, b):
+    return x* a + b
+    #return a*x+b
+
+#Experimental x and y data points
+xData = np.array([0.1 , 52 , 102 ,152 , 203 , 253  , 306 , 356 , 406 , 456]) #filled water in gram almost equal to volume of the bottle
+yData = np.array([165.53 , 172.23 , 180.06 , 194.52,  208.79 , 231.45 ,257.81 , 298.83 , 369.14 , 517.09 ]) #f:frequency in Hz
+V_0AirVolume = 500 - xData #Ann:Dichte_Wasser = 1 deswegen direkt abgezogen
+OneDividedByV_0AirVolume = 1 / V_0AirVolume
+yData2 = yData*yData
+print(yData2)
+
+xError =0.0005 #0.5g Küchenwaage Unsicherheit
+
+#Plot experimental data points
+plt.plot(OneDividedByV_0AirVolume, yData2, 'bo', label='experimental-data')
+plt.errorbar(OneDividedByV_0AirVolume, yData2 , xerr= xError, fmt = 'none' ,label='errorBar due to kitchen scale')
+# Initial guess for the parameters
+initialGuess = [1.0,1.0]
+
+#Perform the curve-fit
+popt, pcov = curve_fit(func, OneDividedByV_0AirVolume, yData2, initialGuess)
+print(popt)
+
+#x values for the fitted function
+xFit = np.arange(0.0, 0.03, 0.0001)
+
+
+
+#Plot the fitted function
+plt.plot(xFit, func(xFit, *popt), 'r', label='fit params: a=%5.3f, b=%5.3f' % tuple(popt))
+plt.title('Helmholtz Resonator Bottle 1 (Frequency^2) and (1/Air Volume) Relation')
+plt.xlabel('1/V_0 /ml')
+plt.ylabel('f^2 / Hz^2')
+plt.legend()
+plt.show()
