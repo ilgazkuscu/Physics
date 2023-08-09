3f88a152a31c6383473586d8469c9dc0a850ce2a
ilgazkuscu <40474649+ilgazkuscu@users.noreply.github.com>
2023-08-09 23:12:07 +0200

    Add files via upload

--- TV2 Tekrar.py
+++ TV2 Tekrar.py
@@ -11,34 +11,36 @@ def func(x, a, b):
 
 #Experimental x and y data points
 
-betaAngle = np.array([67,60,50,38,27,18,8,-4,-13]) #beta angle from torsion in degree upper
-alphaAngle = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90]) #alpha angle from torsion in degree
+betaDiff =np.array([0,-8,-7,-10,-12,-9,-9,-10,-12,-13])
+betaNet = np.array([-8,-15,-25,-37,-48,-57,-67,-79,-92])
+betaAngle = np.array([75,67,60,50,38,27,18,8,-4,-13]) #beta angle from torsion in degree upper
+alphaAngle = np.array([0,10, 20, 30, 40, 50, 60, 70, 80, 90]) #alpha angle from torsion in degree
 xError =7 #Error of Phi due to 4 degrees of error of both alpha and beta (via Gaussian)
 Beta_0 = 75 #0-Setting for the beta angle, const. in degree
 Delta_Alpha_0 = 4# the alpha angle unc.
-betaAngleNet = Beta_0 - betaAngle
+betaAngleNet = betaAngle - Beta_0
+phi = alphaAngle - betaDiff
 
-
-Phi = alphaAngle - (Beta_0 -betaAngle) #beta angle from torsion in degree upper #Phi:=Torsion angle
+Phi = alphaAngle - betaAngleNet #beta angle from torsion in degree upper #Phi:=Torsion angle
 yDataSin =np.sin(np.deg2rad(alphaAngle))#sin(alpha) from torsion cable
 
 
 print(Phi)
 print(yDataSin)
-
-
+print(betaAngleNet)
+print(phi)
 #Plot experimental data points
-plt.plot(Phi, (yDataSin), 'bo', label='experimental-data')
-plt.errorbar(Phi, (yDataSin) , xerr= xError, fmt = 'none' ,label='errorBar v.Winkel')
+plt.plot(phi, (yDataSin), 'bo', label='experimental-data')
+plt.errorbar(phi, (yDataSin) , xerr= xError, fmt = 'none' ,label='errorBar v.Winkel')
 # Initial guess for the parameters
-initialGuess = [1.0,1.0]
+initialGuess = [1.0,0]
 
 #Perform the curve-fit
-popt, pcov = curve_fit(func, Phi, abs(yDataSin), initialGuess)
+popt, pcov = curve_fit(func, phi, abs(yDataSin), initialGuess)
 print(popt)
 
 #x values for the fitted function
-xFit = np.arange(0, 5,1)
+xFit = np.arange(0, 105,0.1)
 
 
 
